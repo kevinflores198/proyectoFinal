8803b2a2c39f268c8662bd4022ce0cc106cc5d23
ManuelErmini <113480862+ManuelErmini@users.noreply.github.com>
2023-08-10 19:44:20 -0300

    Entidad ReportComentario y su Enum

--- src/main/java/com/socialFashion/proyectoFinal/Entidades/hola.java
+++
@@ -1,5 +0,0 @@
-package com.socialFashion.proyectoFinal.Entidades;
-
-public class hola {
-    
-}
---
+++ src/main/java/com/socialFashion/proyectoFinal/Enumeraciones/ReportsComentario.java
@@ -0,0 +1,11 @@
+package com.socialFashion.proyectoFinal.Enumeraciones;
+
+public enum ReportsComentario {
+    SPAM,
+    SEXUAL,
+    INAPROPIADO,
+    VIOLENCIA,
+    BULLYNG,
+    RACISMO,
+    OTRO;
+}
