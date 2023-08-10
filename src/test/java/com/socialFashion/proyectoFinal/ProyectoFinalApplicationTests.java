72e89265e018adbbcde927c8fb267d9bba585d16
kevinflores198 <kevinfloresg1998@gmail.com>
2023-08-09 23:44:35 -0300

    spring initializr

---
+++ src/test/java/com/socialFashion/proyectoFinal/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialFashion.proyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
