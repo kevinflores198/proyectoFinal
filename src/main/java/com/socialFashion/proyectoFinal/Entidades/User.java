e8f36a96dd1cfe74ee10f58e86bcae05163e7cef
Luca P <lucaexequiel@gmail.com>
2023-08-10 18:38:53 -0300

    Update User.java

--- src/main/java/com/socialFashion/proyectoFinal/Entidades/User.java
+++ src/main/java/com/socialFashion/proyectoFinal/Entidades/User.java
@@ -8,6 +8,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import org.hibernate.annotations.GenericGenerator;
 
 import com.socialFashion.proyectoFinal.Enumeraciones.Role;
