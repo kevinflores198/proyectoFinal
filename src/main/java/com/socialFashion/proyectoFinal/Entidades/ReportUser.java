3913113e01d24cf2a0dadce4ef8dd21d07daf874
AriSueyro <asueyro@gmail.com>
2023-08-11 21:09:36 -0300

    Creacion de entidad.-
    Creacion de Entidad ReportUser
    Creacion de ENUM ReportsUser
    Modificacion de Autogeneracion de IdReport(comentario)

---
+++ src/main/java/com/socialFashion/proyectoFinal/Entidades/ReportUser.java
@@ -0,0 +1,93 @@
+
+package com.socialFashion.proyectoFinal.Entidades;
+
+import com.socialFashion.proyectoFinal.Enumeraciones.ReportsUser;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+
+public class ReportUser {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String idReporte;
+    
+    private String idUser;
+    private String idUserReported;
+    private String reason;
+    private boolean alta;
+    
+    @Enumerated(EnumType.STRING)
+    private ReportsUser typeReport;
+
+    public ReportUser() {
+    }
+
+    public ReportUser(String idReporte, String idUser, String idUserReported, String reason, boolean alta, ReportsUser typeReport) {
+        this.idReporte = idReporte;
+        this.idUser = idUser;
+        this.idUserReported = idUserReported;
+        this.reason = reason;
+        this.alta = alta;
+        this.typeReport = typeReport;
+    }
+
+    public String getIdReporte() {
+        return idReporte;
+    }
+
+    public void setIdReporte(String idReporte) {
+        this.idReporte = idReporte;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getIdUserReported() {
+        return idUserReported;
+    }
+
+    public void setIdUserReported(String idUserReported) {
+        this.idUserReported = idUserReported;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public boolean isAlta() {
+        return alta;
+    }
+
+    public void setAlta(boolean alta) {
+        this.alta = alta;
+    }
+
+    public ReportsUser getTypeReport() {
+        return typeReport;
+    }
+
+    public void setTypeReport(ReportsUser typeReport) {
+        this.typeReport = typeReport;
+    }
+    
+    
+    
+    
+    
+}
