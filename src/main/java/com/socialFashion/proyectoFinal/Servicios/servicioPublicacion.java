ad1eaeca9300540c977a3658a2de27520b904a08
ManuelErmini <113480862+ManuelErmini@users.noreply.github.com>
2023-08-16 18:06:09 -0300

    @Service

--- src/main/java/com/socialFashion/proyectoFinal/Servicios/servicioPublicacion.java
+++ src/main/java/com/socialFashion/proyectoFinal/Servicios/servicioPublicacion.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +18,7 @@ import com.socialFashion.proyectoFinal.Repositorios.UserRepository;
 import com.socialFashion.proyectoFinal.Repositorios.repositorioPublicacion;
 
 
+@Service
 public class servicioPublicacion {
     @Autowired
     private servicioImagen sImg;
