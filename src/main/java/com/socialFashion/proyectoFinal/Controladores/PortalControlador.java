537d701d89ecc9dcb86e6ba6b7f35c5dd3fb8636
ManuelErmini <113480862+ManuelErmini@users.noreply.github.com>
2023-08-16 20:08:38 -0300

    Add AdminControlador y ROLE USER
    agrego metodo de que si el userv tiene algun rol t lleve a su vista

--- src/main/java/com/socialFashion/proyectoFinal/Controladores/PortalControlador.java
+++ src/main/java/com/socialFashion/proyectoFinal/Controladores/PortalControlador.java
@@ -2,7 +2,10 @@ package com.socialFashion.proyectoFinal.Controladores;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.socialFashion.proyectoFinal.Entidades.Usuario;
 import com.socialFashion.proyectoFinal.Exceptions.MiException;
 import com.socialFashion.proyectoFinal.Servicios.ServicioUsuario;
 
@@ -68,8 +72,23 @@ public class PortalControlador {
         return "main.html";
     }
 
-    
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @GetMapping("/main")
+    public String inicio(HttpSession session) {
+        
+        Usuario logueado = (Usuario) session.getAttribute("usuariosession");
+        
+        if (logueado.getRole().toString().equals("ADMIN")) {
+            return "redirect:/admin/dashboard";
+        }
+        
+           return "main.html";
+    }
 
+    //PERFIL VISTA (PENDIENTE)
+
+    //ACTUALIZAR PERFIL VISTA (PENDIENTE)
+    
 
 
 }
