8dc5431c914609cf48bc4f297ea79982f3b3cb32
AriSueyro <asueyro@gmail.com>
2023-08-17 11:24:56 -0300

    Repos
    Repositorio ReporteUsuario
    Repositorio ReporteComentario

--- src/main/java/com/socialFashion/proyectoFinal/Repositorios/RepositorioUsuario.java
+++ src/main/java/com/socialFashion/proyectoFinal/Repositorios/RepositorioUsuario.java
@@ -4,11 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.socialFashion.proyectoFinal.Entidades.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * repositorioUsuario
  */
 @Repository
 public interface RepositorioUsuario extends JpaRepository<Usuario, String>{
+    
+    @Query("SELECT u FROM Usuario u WHERE u.id = :id")
+    public Usuario BuscarUsuarioPorID(@Param("id")String id);
     
 } 
